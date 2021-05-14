705c015e278f2c9721ba6e0cf0217d5fda61b13e
HyrKG <end_kg@126.com>
2021-05-15 01:48:54 +0800

    add bStats Metrics

--- FastSpigot/src/main/java/cn/hyrkg/fastspigot/spigotplugin/PluginFastSpigot.java
+++ FastSpigot/src/main/java/cn/hyrkg/fastspigot/spigotplugin/PluginFastSpigot.java
@@ -2,6 +2,7 @@ package cn.hyrkg.fastspigot.spigotplugin;
 
 
 import cn.hyrkg.fastspigot.fast.easygui.EasyGuiHandler;
+import cn.hyrkg.fastspigot.spigotplugin.bstats.Metrics;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.PluginLoadOrder;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -14,6 +15,8 @@ public class PluginFastSpigot extends JavaPlugin {
     @Override
     public void onEnable() {
         EasyGuiHandler.init(this);
+        Metrics metrics = new Metrics(this, 11356);
+
         getServer().getConsoleSender().sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "[FASTSPIGOT] SUPPORT ENABLED!");
     }
 }
