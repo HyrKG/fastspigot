451df24f9d568da8aeb041586e8b8e302ed3c64b
HyrKG <end_kg@126.com>
2022-12-30 01:31:30 +0800

    chore: fix and version update

--- FastSpigot/src/main/java/cn/hyrkg/fastspigot/fast/forgeui/BaseForgeGui.java
+++ FastSpigot/src/main/java/cn/hyrkg/fastspigot/fast/forgeui/BaseForgeGui.java
@@ -4,19 +4,24 @@ import com.google.gson.JsonObject;
 import lombok.Getter;
 import org.bukkit.entity.Player;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
-
 public class BaseForgeGui extends BaseForgeShareGui {
     @Getter
     private final Player viewer;
 
     public BaseForgeGui(Player viewer, String guiShortName, ForgeGuiHandler guiHandler) {
-        super(guiShortName,guiHandler);
+        super(guiShortName, guiHandler);
         this.viewer = viewer;
         this.addViewer(viewer);
     }
 
+    @Override
+    public void onMessage(Player viewer, JsonObject jsonObject) {
+        onMessage(jsonObject);
+    }
+
+    @Override
+    public void onClose(Player viewer) {
+        onClose();
+    }
 
 }
